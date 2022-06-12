66e09dae90fd07119f78b0b88070957b5338d471
SushyDev <xsushy@hotmail.com>
2022-06-12 17:57:19 +0200

    next

--- src/provider.ts
+++ src/provider.ts
@@ -49,7 +49,7 @@ export const providerSetup: Record<string, ProviderSetup> = {
       images: {
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-        path: '/_vercel/image',
+        path: '/_next/image',
         loader: 'default',
         disableStaticImages: false,
         minimumCacheTTL: 60,
